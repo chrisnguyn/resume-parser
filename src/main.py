a8fda8b1707b0144bbbdbf3258d028c4893b0830
Abdullah Basulaib <basulaib@my.yorku.ca>
2020-09-28 14:26:32 -0400

    implemented PR suggestions

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
-import json
-import requests
 import glob
+import json
 import os
+import requests
 
 print("Scanning for resume(s)...")
 
-files = [f for f in glob.glob("*.pdf")]
+# get all PDF files in current directory
+files = [file for file in glob.glob("*.pdf")]
 if len(files) == 0:
-    print("No PDF found")
+    print("No PDF(s) found")
     quit()
 
 print("Resume(s) found:")
@@ -22,23 +23,28 @@ if not len(files) == 1:  # if more than 1 resume exist
                 break
             else:
                 print("Please enter a number between 1 and " + str(len(files)))
-        except ValueError:
-            print("Please enter a number!")
+        except ValueError:  # catch non-number input
+            print("Please enter a number between 1 and " + str(len(files)))
 
 print("Parsing...")
+
+# if only 1 file, then select automatically
 if len(files) == 1:
-    f = files[0]
+    resume_file_path = files[0]
 else:
-    f = files[option]  # grab user selected
+    resume_file_path = files[option]  # grab user selected file
 
 url = 'https://jobs.lever.co/parseResume'
-resume = open(f, 'rb')
+resume = open(resume_file_path, 'rb')
 
 response = requests.post(url, files={'resume': resume})
 parsed_resume = json.dumps(response.json(), indent=4)
 
-f = os.path.splitext(f)[0] + ".txt"  # remove.pdf and add .txt
+# remove.pdf extension from filename and add .txt
+resume_file_path = os.path.splitext(resume_file_path)[0] + ".txt"
 
-save_response = open(f, 'w')
+save_response = open(resume_file_path, 'w')
 if save_response.write(parsed_resume):
-    print("Resume parsed as: " + f)
+    print("Resume parsed as: " + resume_file_path)
+else:
+    print("Error! Please try again")  # in case anything goes wrong
