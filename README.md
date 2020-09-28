ad552574861e4b9a71e3066d9a03592d666845d2
Christopher Nguyen <chrisngyn99@gmail.com>
2020-09-28 16:56:07 -0400

    cleanin up da code

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # resume-parser
-Using Lever's API to parse resumes.
+Simple script made with Lever's API to parse resumes.
 
 ## Configuration
-1. Add your resume into the `src/` folder as a PDF
-    
-2. Navigate to the `src/` folder in terminal and run `python3 main.py`
+1. Clone the project, navigate into the directory, and run `pip3 install requests`
 
-3. Output will be `<name of file>.txt`
+2. Add your resume(s) into the `src/` folder (.pdf files only)
+    
+3. Navigate to the `src/` folder in terminal and run `python3 main.py`
+
+4. Output will be in the `src/` directory as `${your_file_name}.txt`
--- src/main.py
+++ src/main.py
@@ -3,48 +3,43 @@ import json
 import os
 import requests
 
-print("Scanning for resume(s)...")
 
-# get all PDF files in current directory
-files = [file for file in glob.glob("*.pdf")]
+print('Scanning for resume(s)...')
+
+files = [file for file in glob.glob('*.pdf')]
+
 if len(files) == 0:
-    print("No PDF(s) found")
-    quit()
+    print('No files found - please move files inside the src/ directory.')
+    exit()
 
-print("Resume(s) found:")
+print('Resume(s) found...')
+
 for index, pdf in enumerate(files):
-    print(str(index+1) + ". " + pdf)
+    print(str(index + 1) + " - " + pdf)
 
-if not len(files) == 1:  # if more than 1 resume exist
+if len(files) > 1:
     while True:
         try:
-            option = int(input("Select file number: ")) - 1
+            option = int(input('Select file number: ')) - 1
+
             if option in range(0, len(files)):
+                resume_file = files[option]
                 break
             else:
-                print("Please enter a number between 1 and " + str(len(files)))
-        except ValueError:  # catch non-number input
-            print("Please enter a number between 1 and " + str(len(files)))
-
-print("Parsing...")
-
-# if only 1 file, then select automatically
-if len(files) == 1:
-    resume_file_path = files[0]
+                print('Please enter a valid number between 1 and ' + str(len(files)))
+        except ValueError:
+            print("Please enter a valid number between 1 and " + str(len(files)))    
 else:
-    resume_file_path = files[option]  # grab user selected file
+    resume_file = files[0]
 
 url = 'https://jobs.lever.co/parseResume'
-resume = open(resume_file_path, 'rb')
+resume = open(resume_file, 'rb')
+resume_file_path = os.path.splitext(resume_file)[0] + '.txt'
 
 response = requests.post(url, files={'resume': resume})
 parsed_resume = json.dumps(response.json(), indent=4)
 
-# remove.pdf extension from filename and add .txt
-resume_file_path = os.path.splitext(resume_file_path)[0] + ".txt"
-
 save_response = open(resume_file_path, 'w')
-if save_response.write(parsed_resume):
-    print("Resume parsed as: " + resume_file_path)
-else:
-    print("Error! Please try again")  # in case anything goes wrong
+save_response.write(parsed_resume)
+
+print('Output saved as:', resume_file_path)
